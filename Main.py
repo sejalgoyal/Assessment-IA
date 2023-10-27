b199366032d591314e3baa6406a10d79968a4418
Sejal Goyal <sejalgoyal1998@gmail.com>
2023-10-27 15:04:35 +0530

    Adding code changes

--- Main.py
+++ Main.py
@@ -1,20 +1,21 @@
 def main():
-  n = input("Please enter value of N:")
-  if n < 0:
-    raise Exception("Please provide positive value of N")
-  # We will use Dynamic programming here
-  cols = 4
-  rows = n
-  attendance = [[0]*cols]*(rows+1)
-  attendance[0][0] = 1
-  for i in range(1,n+1):
-    attendance[i][0] = sum(attendance[i-1][:4])
-    for j in range(1, cols+1):
-      attendance[i][j] = attendance[i-1][j-1]
-  
-      
+    n = int(input("Please enter value of N:"))
+    if n < 0:
+        raise Exception("Please provide positive value of N")
+    # We will use Dynamic programming here
+    cols = 4
+    rows = n
+    attendance = [[0 for i in range(cols)] for j in range(rows+1)]
+    attendance[0][0] = 1
+    for i in range(1,n+1):
+        attendance[i][0] = sum(attendance[i-1][:4])
+        for j in range(1, cols):
+            attendance[i][j] = attendance[i-1][j-1]
+    ans_1 = str(sum(attendance[n][1:]))
+    ans_2 = str(sum(attendance[n]))
+    print(ans_1+"/"+ans_2)
   
   
 
-def __name__ == "__main__":
-  main()
+if __name__=="__main__":
+    main()
